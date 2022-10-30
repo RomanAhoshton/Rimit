f13c71983bb8d82be505f09d553aef4b74e6fa2f
RomanAhoshton <109685884+RomanAhoshton@users.noreply.github.com>
2022-10-30 18:12:15 +0200

    change form and menu open lock

--- js/script.js
+++ js/script.js
@@ -1,15 +1,36 @@
 const burger_icon =document.querySelector(".burger_icon");
 const Close = document.getElementById("close");
 const menu_nav=document.querySelector(".menu_nav");
+const body=document.querySelector(".body1");
+
 
 burger_icon.addEventListener("click",()=>{
     menu_nav.classList.add("active");
+   
+
+});
+
+Close.addEventListener("click",()=>{
+    menu_nav.classList.remove("active");
+    
 
 });
 
 
+
+
+
+burger_icon.addEventListener("click",()=>{
+    body.classList.add("lock");
+    
+   
+
+});
+
 Close.addEventListener("click",()=>{
-    menu_nav.classList.remove("active");
+    body.classList.remove("lock");
+    
+    
 
 });
 
