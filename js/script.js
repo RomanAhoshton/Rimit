63450934ff218a1d782873a8cb0e626b139a82ca
RomanAhoshton <109685884+RomanAhoshton@users.noreply.github.com>
2022-11-10 01:04:03 +0200

    change input

--- js/script.js
+++ js/script.js
@@ -2,36 +2,28 @@ const burger_icon =document.querySelector(".burger_icon");
 const Close = document.getElementById("close");
 const menu_nav=document.querySelector(".menu_nav");
 const body=document.querySelector(".body1");
+const footer_input=document.querySelector(".footer_input");
 
+footer_input.addEventListener("click",()=>{
+    footer_input.removeAttribute("placeholder")
+
+})
 
 burger_icon.addEventListener("click",()=>{
     menu_nav.classList.add("active");
    
-
 });
 
 Close.addEventListener("click",()=>{
     menu_nav.classList.remove("active");
-    
-
 });
-
-
-
-
 
 burger_icon.addEventListener("click",()=>{
     body.classList.add("lock");
-    
-   
-
 });
 
 Close.addEventListener("click",()=>{
     body.classList.remove("lock");
-    
-    
-
 });
 
 const input=document.getElementById("input");
